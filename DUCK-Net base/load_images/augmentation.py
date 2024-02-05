4efc6c1f3940e9bf76c8615eaf9d36a063db3cf6
khanhdat111 <128824280+khanhdat111@users.noreply.github.com>
2024-02-06 01:23:06 +0700

    Update augmentation.py

--- DUCK-Net base/load_images/augmentation.py
+++ DUCK-Net base/load_images/augmentation.py
@@ -1,1 +1,19 @@
+# Defining the augmentations
+import albumentations as albu
+aug_train = albu.Compose([
+    albu.HorizontalFlip(),
+    albu.VerticalFlip(),
+    albu.ColorJitter(brightness=(0.6,1.6), contrast=0.2, saturation=0.1, hue=0.01, always_apply=True),
+    albu.Affine(scale=(0.5,1.5), translate_percent=(-0.125,0.125), rotate=(-180,180), shear=(-22.5,22), always_apply=True),
+])
 
+def augment_images():
+    x_train_out = []
+    y_train_out = []
+
+    for i in range (len(x_train)):
+        ug = aug_train(image=x_train[i], mask=y_train[i])
+        x_train_out.append(ug['image'])  
+        y_train_out.append(ug['mask'])
+
+    return np.array(x_train_out), np.array(y_train_out)
