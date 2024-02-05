1037722dbf9e6991c648f33eaa90642c8497241d
khanhdat111 <128824280+khanhdat111@users.noreply.github.com>
2024-02-06 00:14:13 +0700

    Update load_images.py

--- DUCK-Net base/load_images/load_images.py
+++ DUCK-Net base/load_images/load_images.py
@@ -1,65 +1,33 @@
 import glob
-
 import numpy as np
 from PIL import Image
-from skimage.io import imread
+from skimage.transform import resize
 from tqdm import tqdm
 
-folder_path = "/kaggle/input/cvc-clinic-png/"  # Add the path to your data directory
-
+folder_path = "/kaggle/input/cvc-clinic-png/"  # Adjust the path to your data directory accordingly
 
 def load_data(img_height, img_width, images_to_be_loaded, dataset):
-    IMAGES_PATH = folder_path + 'Original/'
-    MASKS_PATH = folder_path + 'Ground Truth/'
+    IMAGES_PATH = f"{folder_path}Original/"
+    MASKS_PATH = f"{folder_path}Ground Truth/"
 
-    if dataset == 'kvasir':
-        train_ids = glob.glob(IMAGES_PATH + "*.jpg")
+    # Simplify dataset pattern matching
+    pattern = "*.jpg" if dataset == 'kvasir' else "*.png"
+    train_ids = glob.glob(IMAGES_PATH + pattern)
 
-    if dataset == 'cvc-clinicdb':
-        train_ids = glob.glob(IMAGES_PATH + "*.png")
-        #train_ids = train_ids[:30]
-
-    if dataset == 'cvc-colondb' or dataset == 'etis-laribpolypdb':
-        train_ids = glob.glob(IMAGES_PATH + "*.png")
-
-    if images_to_be_loaded == -1:
+    # Optionally limit the number of images to be loaded
+    if images_to_be_loaded == -1 or images_to_be_loaded > len(train_ids):
         images_to_be_loaded = len(train_ids)
-        print(images_to_be_loaded)
 
     X_train = np.zeros((images_to_be_loaded, img_height, img_width, 3), dtype=np.float32)
-    Y_train = np.zeros((images_to_be_loaded, img_height, img_width), dtype=np.uint8)
+    Y_train = np.zeros((images_to_be_loaded, img_height, img_width, 1), dtype=np.bool_)
 
-    print('Resizing training images and masks: ' + str(images_to_be_loaded))
-    for n, id_ in tqdm(enumerate(train_ids)):
-        if n == images_to_be_loaded:
-            break
+    print(f'Resizing training images and masks: {images_to_be_loaded}')
+    for n, id_ in tqdm(enumerate(train_ids[:images_to_be_loaded]), total=images_to_be_loaded):
+        image = np.array(Image.open(id_).resize((img_width, img_height)))
+        mask_path = id_.replace("Original", "Ground Truth").replace(".jpg", ".png").replace(".png", ".png")
+        mask = np.array(Image.open(mask_path).resize((img_width, img_height), resample=Image.LANCZOS))
 
-        image_path = id_
-        mask_path = image_path.replace("images", "masks")
-
-        image = imread(image_path)
-        mask_ = imread(mask_path)
-
-        mask = np.zeros((img_height, img_width), dtype=np.bool_)
-
-        pillow_image = Image.fromarray(image)
-
-        pillow_image = pillow_image.resize((img_height, img_width))
-        image = np.array(pillow_image)
-
-        X_train[n] = image / 255
-
-        pillow_mask = Image.fromarray(mask_)
-        pillow_mask = pillow_mask.resize((img_height, img_width), resample=Image.LANCZOS)
-        mask_ = np.array(pillow_mask)
-
-        for i in range(img_height):
-            for j in range(img_width):
-                if (mask_[i, j] >= 127).all():
-                    mask[i, j] = 1
-
-        Y_train[n] = mask
-
-    Y_train = np.expand_dims(Y_train, axis=-1)
+        X_train[n] = image / 255.0
+        Y_train[n] = np.expand_dims(mask >= 127, axis=-1)
 
     return X_train, Y_train
