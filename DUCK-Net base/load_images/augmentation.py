9f73b9ec075e52f4d6b8026efd1d628a44e05e6f
khanhdat111 <128824280+khanhdat111@users.noreply.github.com>
2024-02-07 00:23:05 +0700

    Update augmentation.py

--- DUCK-Net base/load_images/augmentation.py
+++ DUCK-Net base/load_images/augmentation.py
@@ -2,27 +2,27 @@
 import albumentations as albu
 import numpy as np
 
-# aug_train = albu.Compose([
-#     albu.HorizontalFlip(),
-#     albu.VerticalFlip(),
-#     albu.ColorJitter(brightness=(0.6,1.6), contrast=0.2, saturation=0.1, hue=0.01, always_apply=True),
-#     albu.Affine(scale=(0.5,1.5), translate_percent=(-0.125,0.125), rotate=(-180,180), shear=(-22.5,22), always_apply=True),
-# ])
-
 aug_train = albu.Compose([
-    albu.HorizontalFlip(p=0.5),
-    albu.VerticalFlip(p=0.5),
-    albu.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.02, p=0.9),
-    albu.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.9),
-    albu.GaussNoise(var_limit=(10.0, 50.0), p=0.5),
-    albu.ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=0.5),
-    albu.RandomGamma(gamma_limit=(80, 120), p=0.9),
-    #albu.CLAHE(clip_limit=2.0, tile_grid_size=(8, 8), p=0.5),
-    albu.Affine(scale=(0.8,1.2), translate_percent=(-0.2,0.2), rotate=(-180,180), shear=(-8,8), p=0.9),
-    albu.CoarseDropout(max_holes=8, max_height=8, max_width=8, min_holes=1, fill_value=0, p=0.5),
-    albu.RandomSizedCrop(min_max_height=(256, 256), height=512, width=512, p=0.5),
-    albu.Resize(height=256, width=256, p=1.0)
+    albu.HorizontalFlip(),
+    albu.VerticalFlip(),
+    albu.ColorJitter(brightness=(0.6,1.6), contrast=0.2, saturation=0.1, hue=0.01, always_apply=True),
+    albu.Affine(scale=(0.5,1.5), translate_percent=(-0.125,0.125), rotate=(-180,180), shear=(-22.5,22), always_apply=True),
 ])
+
+# aug_train = albu.Compose([
+#     albu.HorizontalFlip(p=0.5),
+#     albu.VerticalFlip(p=0.5),
+#     albu.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.02, p=0.9),
+#     albu.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.9),
+#     albu.GaussNoise(var_limit=(10.0, 50.0), p=0.5),
+#     albu.ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=0.5),
+#     albu.RandomGamma(gamma_limit=(80, 120), p=0.9),
+#     #albu.CLAHE(clip_limit=2.0, tile_grid_size=(8, 8), p=0.5),
+#     albu.Affine(scale=(0.8,1.2), translate_percent=(-0.2,0.2), rotate=(-180,180), shear=(-8,8), p=0.9),
+#     albu.CoarseDropout(max_holes=8, max_height=8, max_width=8, min_holes=1, fill_value=0, p=0.5),
+#     albu.RandomSizedCrop(min_max_height=(256, 256), height=512, width=512, p=0.5),
+#     albu.Resize(height=256, width=256, p=1.0)
+# ])
 
 def augment_images(x_train,y_train):
     x_train_out = []
